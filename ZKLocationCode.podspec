666611232c17dc225760dbeb22642238ea9a3879
Adam <614451892@qq.com>
2017-02-03 10:31:53 +0800

    暂时注释

--- ZKLocationCode.podspec
+++ ZKLocationCode.podspec
@@ -7,9 +7,9 @@ Pod::Spec.new do |s|
   s.social_media_url   = "http://www.weibo.com/u/5527441819"
   s.license= { :type => "MIT", :file => "LICENSE" }
   s.author       = { "Adam" => "614451892@11.com" }
-  s.source       = { :git => "https://github.com/zhenkunew123/ZKLocationCode.git", :tag => s.version }
+  s.source       = { :git => "https://github.com/zhenkunew123/ZKLocationCode.git", :tag => '0.0.1' }
   s.source_files = "ZKLocationCode/*.{h,m}"
-  s.ios.deployment_target = '6.0'
+  s.ios.deployment_target = '8.0'
   s.frameworks   = 'UIKit', 'Foundation'
   s.requires_arc = true
 
--- ZKLocationCode/ZKLocationCode.m
+++ ZKLocationCode/ZKLocationCode.m
@@ -259,19 +259,19 @@ const void * ACTIONNAME = "ACTIONNAME";
 @end
 
 @interface UITableView ()
-- (void)_userSelectRowAtPendingSelectionIndexPath:(id)obj;
+//- (void)_userSelectRowAtPendingSelectionIndexPath:(id)obj;
 @end
 
 @implementation UITableView (zk_extension)
 
-+ (void)load {
-    [ZKLocationCode swizzleInstanceMethod:NSClassFromString(@"UITableView") originSelector:@selector(_userSelectRowAtPendingSelectionIndexPath:) otherSelector:@selector(zk_userSelectRowAtPendingSelectionIndexPath:)];
-}
-
-- (void)zk_userSelectRowAtPendingSelectionIndexPath:(id)obj {
-    [self zk_userSelectRowAtPendingSelectionIndexPath:obj];
-    [[ZKLocationCode sharedLocationCode] didSelCell:self.superview action:@"didSelectRowAtIndexPath"];
-}
+//+ (void)load {
+//    [ZKLocationCode swizzleInstanceMethod:NSClassFromString(@"UITableView") originSelector:@selector(_userSelectRowAtPendingSelectionIndexPath:) otherSelector:@selector(zk_userSelectRowAtPendingSelectionIndexPath:)];
+//}
+//
+//- (void)zk_userSelectRowAtPendingSelectionIndexPath:(id)obj {
+//    [self zk_userSelectRowAtPendingSelectionIndexPath:obj];
+//    [[ZKLocationCode sharedLocationCode] didSelCell:self.superview action:@"didSelectRowAtIndexPath"];
+//}
 
 @end
 
